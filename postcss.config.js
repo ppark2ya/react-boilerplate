43d500ac402afe863d34f012268bd802f11d35c6
jtpark <vigabhan1231@gmail.com>
2022-03-13 17:37:13 +0900

    chore: git hook 추가
    git commit을 일관성 있게 유지하기 위한 설정을 추가함
    
    - husky, lint-staged 를 이용해서 commit 전에 linting, prettier 실행
    - commit message convetion rules 추가

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
+  map: isDev ? 'inline' : false,
   plugins: {
+    'postcss-import': {},
     'postcss-flexbugs-fixes': {},
+    /**
+     * @see https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-focus-visible#readme
+     * @see https://marshallku.com/web/tips/focus-visible%EB%A1%9C-%EC%A0%91%EA%B7%BC%EC%84%B1-%EB%86%92%EC%9D%B4%EA%B8%B0
+     */
+    'postcss-focus-visible': {},
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009',
@@ -12,6 +21,6 @@ module.exports = {
     },
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    cssnano: !isDev ? {} : false,
   },
 };

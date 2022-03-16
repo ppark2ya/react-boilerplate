69327e5d74c5acc5d18816db2eab26e823dbef2e
jtpark <vigabhan1231@gmail.com>
2022-03-16 19:18:29 +0900

    chore: npm to yarn berry
    zero install을 위해서 npm에서 yarn berry로 마이그레이션함

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './global.pcss';
 import App from './App';
 
-const root = ReactDOM.createRoot(
+const root = (ReactDOM as any).createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(<App />);

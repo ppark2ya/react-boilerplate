4262bf207fe9231f76b805693bc1a00100a54f7f
jtpark <vigabhan1231@gmail.com>
2022-03-17 22:48:22 +0900

    feat: url-import에서 지원하는 파일 확장자 type 추가
    static 파일들을 import 할 때 필요한 declare module *.any 추가

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,5 +1,12 @@
 declare module '*.scss';
+declare module '*.css';
 declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.woff';
+declare module '*.woff2';
 
 declare const process: {
   env: Record<string, string>;
